f1785109b4bdeab6398d9d591592ce1272363231
peleg2210 <45825608+pkd2210@users.noreply.github.com>
2025-12-21 22:01:04 +0200

    update baseurl

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -4,7 +4,7 @@ export const GET = async () => {
     return new Response(null, {
         status: 303,
         headers: {
-            location: `https://auth.hackclub.com/oauth/authorize?client_id=${config["hca-client-id"]}&redirect_uri=http://localhost:5173/api/callback&response_type=code&scope=email+name+slack_id+verification_status`
+            location: `https://auth.hackclub.com/oauth/authorize?client_id=${config["hca-client-id"]}&redirect_uri=${config["base-url"]}/api/callback&response_type=code&scope=email+name+slack_id+verification_status`
         }
     });
 };
