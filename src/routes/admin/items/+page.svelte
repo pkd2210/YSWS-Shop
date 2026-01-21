3f910c979169d775303cc48f2d0b919d2a038c58
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-01-21 10:10:49 +0000

    Add some menus in the admin panel

---
+++ src/routes/admin/items/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import config from '$lib/stores/config.json';
+    let { data } = $props();
+</script>
+
+<div class="flex flex-col gap-4">
+    <section class="flex flex-col items-center justify-center gap-4">
+        <div class="title">{config["ysws-name"]}'s Item Management</div>
+        <div class="flex flex-wrap items-center justify-center gap-4">
+            <a href="{config['url-base']}/admin/items/new" class="px-4 py-2 bg-[var(--theme-color)] text-[var(--background-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow">Create a new item</a>
+        </div>
+    </section>
+</div>
