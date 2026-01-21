ed4aa4169301d0a3f57a41dbfb7f1f8883335cf5
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-01-21 10:51:59 +0000

    Update orders to fetch info directly from the api

--- src/routes/shop/orders/api/+server.ts
+++ src/routes/shop/orders/api/+server.ts
@@ -6,9 +6,20 @@ dotenv.config();
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_ACCESS_TOKEN }).base(process.env.BASE_ID!);
 
-export async function GET({ params, parent }) {
-    const data = await parent();
-    const userSlackId = data.user?.identity?.slack_id;
+export async function GET({ cookies }) {
+    const access_token = cookies.get('hca_access_token');
+    
+    if (!access_token) {
+        return json([]);
+    }
+
+    const response = await fetch('https://auth.hackclub.com/api/v1/me', {
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        }
+    });
+    const userData = await response.json();
+    const userSlackId = userData.identity?.slack_id;
 
     if (!userSlackId) {
         return json([]);
